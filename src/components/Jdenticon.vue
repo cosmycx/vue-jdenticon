ca355185acfc5bc7ada3ae732efc4989bca0f22a
Cosmin Potocean <cosmycx@gmail.com>
2019-01-23 11:19:13 -0500

    first commit

---
+++ src/components/Jdenticon.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+      Input:
+      <input @input="onInput" />
+    </div>
+
+    <div>
+        Output:
+        <div v-html="identicon" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import jd from 'jdenticon'
+
+  export default {
+    name: 'Jdenticon',
+    data: () => {
+      return {
+        textInput: '',
+      }
+    },
+    // .data
+    methods: {
+      onInput(e) {
+        this.textInput = e.target.value
+      }
+    },// .methods
+    computed: {
+      identicon() {
+        return jd.toSvg(this.textInput, 200);
+      },
+    }, // .computed
+  }// .export
+
+</script>
+
+<style>
+</style>
